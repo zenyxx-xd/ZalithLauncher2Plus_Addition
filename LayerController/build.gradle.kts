7ceff4e86bf3c4c7dd0c5314c55d9aef065aa90f
root <root@localhost.localdomain>
2026-08-18 18:32:31 +0000

    build(layer-controller): add androidx.foundation dependency for detectTapGestures

--- LayerController/build.gradle.kts
+++ LayerController/build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.foundation)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     //Project
